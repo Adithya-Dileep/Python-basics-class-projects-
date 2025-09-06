d517d04e8a53146d4e5f208ca4682ba2db6b239c
Adithya-Dileep <Shugyo23@gmail.com>
2025-09-06 13:19:44 +0400

    Add files via upload

---
+++ Getting start with Programing/print.py
@@ -0,0 +1,4 @@
+print('Welcome to Python Programing')
+print(1578)
+a=10
+print('Value of a is', a)
---
+++ Intro to Python/Main.py
@@ -0,0 +1,6 @@
+#Write Python code to add 2 numbers
+a=15
+b=10
+c=0
+c=a+b
+print ('the sum is',c) 
