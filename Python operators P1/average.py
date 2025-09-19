083c9f941c277ea9250761d7fc470bb342a9b793
Adithya-Dileep <Shugyo23@gmail.com>
2025-09-19 16:20:50 +0400

    Add files via upload

---
+++ Python operators P1/average.py
@@ -0,0 +1,9 @@
+#To Write a Program to Find the Average of 5 Numbers
+num1 = 35
+num2 = 78
+num3 = 99
+num4 = 23 
+num5 = 47
+sum = num1+num2+num3+num4+num5
+average = sum/5
+print (average)
---
+++ Python operators P1/percentage.py
@@ -0,0 +1,9 @@
+#To write a program to find the percentage of 5 subjects
+English=68
+Math=89
+Physics=45
+Chemistry=56
+Biology=99
+total= English=Math+Physics+Chemistry+Biology
+percentage=total/500*100
+print('The percentage is : ', percentage)
